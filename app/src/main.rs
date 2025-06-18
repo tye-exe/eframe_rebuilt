2f288aaab24e2bc949e5361891e9dd2d9d5874a5
tye-exe <tye@mailbox.org>
2025-06-18 14:20:10 +0100

    Combined "threaded_app" into "app"
    The code can be modified to changed if it uses the main thread or secondary thread.

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,25 +1,59 @@
 use std::time::Duration;
 
 fn main() {
-    println!("Hello, world!");
     env_logger::init();
 
     for _ in 0..2 {
-        eframe_rebuilt::run_native(
-            "test",
-            Default::default(),
-            Box::new(|_| Ok(Box::new(MyApp {}))),
-        )
-        .unwrap();
-
+        println!("Opened");
+        single_thread_gui();
+        println!("Closed");
         std::thread::sleep(Duration::from_secs(2));
     }
 }
 
-struct MyApp {}
+fn single_thread_gui() {
+    eframe_rebuilt::run_native(
+        "test",
+        Default::default(),
+        Box::new(|_| Ok(Box::new(MyApp::default()))),
+    )
+    .unwrap();
+}
+
+fn multi_thread_gui() {
+    std::thread::spawn(move || {
+        let options = eframe_rebuilt::epi::NativeOptions {
+            event_loop_builder: Some(Box::new(move |builder| {
+                use winit::platform::wayland::EventLoopBuilderExtWayland;
+
+                builder.with_any_thread(true);
+            })),
+            ..Default::default()
+        };
+
+        eframe_rebuilt::run_native(
+            "test",
+            options,
+            Box::new(|_| Ok(Box::new(MyApp::default()))),
+        )
+        .unwrap();
+    })
+    .join()
+    .unwrap();
+}
+
+#[derive(Default)]
+struct MyApp {
+    ticks: u8,
+}
 
 impl eframe_rebuilt::epi::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe_rebuilt::epi::Frame) {
-        //
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe_rebuilt::epi::Frame) {
+        if self.ticks == 2 {
+            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+        }
+
+        self.ticks += 1;
+        ctx.request_repaint();
     }
 }

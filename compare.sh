b6640e11a5f65b6bf994f63fe0eed128d5bdc379
tye-exe <tye@mailbox.org>
2025-06-18 14:56:43 +0100

    Cargo output is silenced when comparing outputs

--- compare.sh
+++ compare.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-RUST_LOG=debug cargo run -- main &> out_main
-RUST_LOG=debug cargo run -- spawn &> out_spawn
+RUST_LOG=debug cargo run -q -- main &> out_main
+RUST_LOG=debug cargo run -q -- spawn &> out_spawn
